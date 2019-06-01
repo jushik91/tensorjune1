edbacd2369b2235c3b59ef5bb11a20553284658a
jushik91 <jushik91@gmail.com>
2019-06-01 18:20:17 +0900

    삼성전자 텍스트 마이닝

---
+++ austin_emma/__init__.py
@@ -0,0 +1,3 @@
+from austin_emma.miner import Emma
+if __name__ == '__main__':
+    Emma()
---
+++ samsung/__init__.py
@@ -0,0 +1,8 @@
+from samsung.miner import Samsung
+if __name__ == '__main__':
+    f = Samsung.read_file()
+    Samsung.extract_hangeul(f)
+
+
+
+
